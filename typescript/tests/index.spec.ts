29569fe68a564c53823390f9c6fd3840faf19cf2
Adam Whaley <adam@industriallogic.com>
2023-02-06 20:51:12 -0600

    cleanup tests

--- typescript/tests/index.spec.ts
+++ typescript/tests/index.spec.ts
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { add } from '../src/index';
+import { add } from '../src';
 
 describe('test', () => {
-    it('should add 1 + 2', () => {
+    it('adds 1 + 2', () => {
         expect(add(1, 2)).to.eq(3);
     });
 });
