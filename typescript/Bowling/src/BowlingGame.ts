5e6dafad7cd2ce9c040a1dfac98186fc1b860b83
Adam Whaley <adam@industriallogic.com>
2023-03-12 11:06:22 -0500

    rename reduce to aggregate to reveal intention

--- typescript/Bowling/src/BowlingGame.ts
+++ typescript/Bowling/src/BowlingGame.ts
@@ -47,7 +47,7 @@ class FrameCollection {
         return this.frames[index];
     }
 
-    reduce(callback: (score: number, frame: Frame, currentIndex: number) => number) {
+    aggregateScore(callback: (score: number, frame: Frame, currentIndex: number) => number) {
         return this.frames.reduce(callback, 0)
     }
 }
@@ -56,7 +56,7 @@ export class BowlingGame {
     private frames: FrameCollection = new FrameCollection([]);
 
     calculateScore(): number {
-        return this.frames.reduce((score, frame, currentIndex) => this.addNextFrame(score, frame, currentIndex))
+        return this.frames.aggregateScore((score, frame, currentIndex) => this.addNextFrame(score, frame, currentIndex))
     }
 
     private addNextFrame(previousScore: number, currentFrame: Frame, currentIndex: number) {
