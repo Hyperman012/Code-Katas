332993b6360b0cece4bc62e655446880cd8adbd7
Adam Whaley <adam@industriallogic.com>
2023-04-01 14:22:28 -0500

    more initial commits

--- typescript/Bowling/src/BowlingGame.ts
+++ typescript/Bowling/src/BowlingGame.ts
@@ -1,100 +1,111 @@
 const STRIKE_PIN_COUNT = 10;
 
 export class Frame {
-    static gutter: Frame = new Frame(0, 0)
-    static strike: Frame = new Frame(10, 0);
+  static gutter: Frame = new Frame(0, 0);
+  static strike: Frame = new Frame(10, 0);
 
-    constructor(private firstRoll: number, private secondRoll: number, private thirdRoll: number = 0) {
-    }
+  constructor(
+    private firstRoll: number,
+    private secondRoll: number,
+    private thirdRoll: number = 0
+  ) {}
 
-    totalPinCount(): number {
-        return this.totalOfNextTwoRolls() + this.thirdRoll;
-    }
+  totalPinCount(): number {
+    return this.totalOfNextTwoRolls() + this.thirdRoll;
+  }
 
+  isSpare() {
+    return !this.isStrike() && this.totalOfNextTwoRolls() === STRIKE_PIN_COUNT;
+  }
 
-    isSpare() {
-        return !this.isStrike()
-            && this.totalOfNextTwoRolls() === STRIKE_PIN_COUNT;
-    }
+  isStrike() {
+    return this.firstRoll === STRIKE_PIN_COUNT;
+  }
 
-    isStrike() {
-        return this.firstRoll === STRIKE_PIN_COUNT;
-    }
+  totalOfNextTwoRolls() {
+    return this.firstRoll + this.secondRoll;
+  }
 
-    totalOfNextTwoRolls() {
-        return this.firstRoll + this.secondRoll;
-    }
-
-    totalOfNextRoll() {
-        return this.firstRoll;
-    }
+  totalOfNextRoll() {
+    return this.firstRoll;
+  }
 }
 
 class FrameCollection {
-    constructor(private frames: Frame[]) {
+  constructor(private frames: Frame[]) {}
 
-    }
+  addFrames(...frames: Frame[]) {
+    this.frames.push(...frames);
+  }
 
-    addFrames(...frames: Frame[]) {
-        this.frames.push(...frames);
-    }
+  isFinalFrame(currentIndex: number) {
+    return currentIndex + 1 === this.frames.length;
+  }
 
-    isFinalFrame(currentIndex: number) {
-        return (currentIndex + 1) === this.frames.length;
-    }
+  get(index: number): Frame {
+    return this.frames[index];
+  }
 
-    get(index: number): Frame {
-        return this.frames[index];
-    }
-
-    aggregateScore(callback: (score: number, frame: Frame, currentIndex: number) => number) {
-        return this.frames.reduce(callback, 0)
-    }
+  aggregateScore(
+    callback: (score: number, frame: Frame, currentIndex: number) => number
+  ) {
+    return this.frames.reduce(callback, 0);
+  }
 }
 
 export class BowlingGame {
-    private frames: FrameCollection = new FrameCollection([]);
+  private frames: FrameCollection = new FrameCollection([]);
 
-    calculateScore(): number {
-        return this.frames.aggregateScore((score, frame, currentIndex) => this.addNextFrame(score, frame, currentIndex))
-    }
+  calculateScore(): number {
+    return this.frames.aggregateScore((score, frame, currentIndex) =>
+      this.addNextFrame(score, frame, currentIndex)
+    );
+  }
 
-    private addNextFrame(previousScore: number, currentFrame: Frame, currentIndex: number) {
-        const baseNewScore = previousScore + currentFrame.totalPinCount();
-        if (this.isFinalFrame(currentIndex)) return baseNewScore
+  private addNextFrame(
+    previousScore: number,
+    currentFrame: Frame,
+    currentIndex: number
+  ) {
+    const baseNewScore = previousScore + currentFrame.totalPinCount();
+    if (this.isFinalFrame(currentIndex)) return baseNewScore;
 
-        return baseNewScore + this.bonusScoreFor(currentFrame, currentIndex);
-    }
+    return baseNewScore + this.bonusScoreFor(currentFrame, currentIndex);
+  }
 
-    private isFinalFrame(currentIndex: number) {
-        return this.frames.isFinalFrame(currentIndex);
-    }
+  private isFinalFrame(currentIndex: number) {
+    return this.frames.isFinalFrame(currentIndex);
+  }
 
-    private bonusScoreFor(currentFrame: Frame, currentIndex: number) {
-        return this.spareBonusScore(currentFrame, currentIndex) + this.strikeBonusScore(currentFrame, currentIndex);
-    }
+  private bonusScoreFor(currentFrame: Frame, currentIndex: number) {
+    return (
+      this.spareBonusScore(currentFrame, currentIndex) +
+      this.strikeBonusScore(currentFrame, currentIndex)
+    );
+  }
 
-    private spareBonusScore(frame: Frame, currentIndex: number) {
-        if (!frame.isSpare()) return 0;
+  private spareBonusScore(frame: Frame, currentIndex: number) {
+    if (!frame.isSpare()) return 0;
 
-        return this.getFrame(currentIndex + 1).totalOfNextRoll();
-    }
+    return this.getFrame(currentIndex + 1).totalOfNextRoll();
+  }
 
-    private strikeBonusScore(frame: Frame, currentIndex: number): number {
-        if (!frame.isStrike()) return 0;
+  private strikeBonusScore(frame: Frame, currentIndex: number): number {
+    if (!frame.isStrike()) return 0;
 
-        const nextFrame = this.getFrame(currentIndex + 1);
-        if (!nextFrame.isStrike() || this.isFinalFrame(currentIndex + 1)) return nextFrame.totalOfNextTwoRolls()
+    const nextFrame = this.getFrame(currentIndex + 1);
+    if (!nextFrame.isStrike() || this.isFinalFrame(currentIndex + 1))
+      return nextFrame.totalOfNextTwoRolls();
 
-        const frameAfterNext = this.getFrame(currentIndex + 2);
-        return nextFrame.totalPinCount() + frameAfterNext.totalOfNextRoll()
-    }
+    const frameAfterNext = this.getFrame(currentIndex + 2);
+    return nextFrame.totalPinCount() + frameAfterNext.totalOfNextRoll();
+  }
 
-    private getFrame(index: number): Frame {
-        return this.frames.get(index);
-    }
+  private getFrame(index: number): Frame {
+    return this.frames.get(index);
+  }
 
-    addFrames(...frames: Frame[]) {
-        this.frames.addFrames(...frames);
-    }
+  addFrames(...frames: Frame[]) {
+    this.frames.addFrames(...frames);
+  }
 }
--- typescript/PascalsTriangle/tests/index.spec.ts
+++ typescript/PascalsTriangle/tests/index.spec.ts
@@ -1,10 +1,10 @@
-import { expect } from 'chai';
-import 'mocha';
+import { expect } from "chai";
+import "mocha";
 
-import { add } from '../src';
+import { add } from "../src";
 
-describe('test', () => {
-    it('adds 1 + 2', () => {
+describe("test", () => {
+    it("adds 1 + 2", () => {
         expect(add(1, 2)).to.eq(3);
     });
 });
