9e9ec828d0b0264540a0f80eaee876455cf0138b
Adam Whaley <adam@industriallogic.com>
2023-04-02 11:01:18 -0500

    remove secondRow duplication

--- typescript/PascalsTriangle/src/index.ts
+++ typescript/PascalsTriangle/src/index.ts
@@ -7,11 +7,9 @@ export function generateTriangle(rowCount: number): Triangle {
     const triangle: Triangle = [];
 
     if (rowCount >= 1) triangle.push(firstRow);
+    if (rowCount >= 2) triangle.push(secondRow);
 
-    if (rowCount === 2) triangle.push(secondRow);
-
-    if (rowCount === 3)
-        triangle.push(secondRow, [1, secondRow[0] + secondRow[1], 1]);
+    if (rowCount === 3) triangle.push([1, secondRow[0] + secondRow[1], 1]);
 
     return triangle;
 }
