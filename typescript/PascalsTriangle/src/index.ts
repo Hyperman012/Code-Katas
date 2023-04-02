7d6242b8e75b6b61c515a3ecf4d755d81efe29a4
Adam Whaley <adam@industriallogic.com>
2023-04-02 10:53:38 -0500

    begin moving to singular return statement

--- typescript/PascalsTriangle/src/index.ts
+++ typescript/PascalsTriangle/src/index.ts
@@ -2,9 +2,14 @@ export const firstRow = [1];
 export const secondRow = [1, 1];
 
 export function generateTriangle(rowCount: number): number[][] {
-    if (rowCount === 0) return [];
+    const triangle: number[][] = [];
+
     if (rowCount === 2) return [firstRow, secondRow];
+
     if (rowCount === 3)
         return [firstRow, secondRow, [1, secondRow[0] + secondRow[1], 1]];
-    return [firstRow];
+
+    if (rowCount === 1) return [firstRow];
+
+    return triangle;
 }
