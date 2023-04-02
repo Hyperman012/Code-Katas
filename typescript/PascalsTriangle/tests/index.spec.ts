d762f1ee4ea6dab57585faa7747faa26e387aa4f
Adam Whaley <adam@industriallogic.com>
2023-04-02 10:44:27 -0500

    remove duplication by using test method

--- typescript/PascalsTriangle/tests/index.spec.ts
+++ typescript/PascalsTriangle/tests/index.spec.ts
@@ -3,18 +3,18 @@ import "mocha";
 
 import { firstRow, generateTriangle, secondRow } from "../src";
 
-function expectCorrectTriangle(rowCount: number, expectedTriangle: number[][]) {
-    expect(generateTriangle(rowCount)).to.deep.eq(expectedTriangle);
-}
-
 describe("Pascal's Triangle", () => {
     it("Produces an empty list for 0", () => {
-        expect(generateTriangle(0)).to.deep.eq([]);
+        expectCorrectTriangle(0, []);
     });
     it("Produces first row for number of rows 1", () => {
-        expect(generateTriangle(1)).to.deep.eq([firstRow]);
+        expectCorrectTriangle(1, [firstRow]);
     });
     it("produces expected rows for input of 2", () => {
         expectCorrectTriangle(2, [firstRow, secondRow]);
     });
 });
+
+function expectCorrectTriangle(rowCount: number, expectedTriangle: number[][]) {
+    expect(generateTriangle(rowCount)).to.deep.eq(expectedTriangle);
+}
