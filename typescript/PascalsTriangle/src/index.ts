e1edb8167c63e997f11ffb71b02df0aa51ad4825
Adam Whaley <adam@industriallogic.com>
2023-04-02 11:05:41 -0500

    remove duplication

--- typescript/PascalsTriangle/src/index.ts
+++ typescript/PascalsTriangle/src/index.ts
@@ -9,9 +9,9 @@ export function generateTriangle(rowCount: number): Triangle {
     if (rowCount >= 1) triangle.push(firstRow);
     if (rowCount >= 2) triangle.push(secondRow);
 
-    if (rowCount === 3) triangle.push([1, secondRow[0] + secondRow[1], 1]);
-    if (rowCount === 4)
-        triangle.push([1, secondRow[0] + secondRow[1], 1], [1, 3, 3, 1]);
+    if (rowCount >= 3) triangle.push([1, secondRow[0] + secondRow[1], 1]);
+
+    if (rowCount === 4) triangle.push([1, 3, 3, 1]);
 
     return triangle;
 }
