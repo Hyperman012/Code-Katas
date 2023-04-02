84836dce0fd853197e583c39cf7eb1edb0b228ba
Adam Whaley <adam@industriallogic.com>
2023-04-02 10:57:46 -0500

    move case 1 to push into array

--- typescript/PascalsTriangle/src/index.ts
+++ typescript/PascalsTriangle/src/index.ts
@@ -6,12 +6,12 @@ export type Triangle = number[][];
 export function generateTriangle(rowCount: number): Triangle {
     const triangle: Triangle = [];
 
+    if (rowCount === 1) triangle.push(firstRow);
+
     if (rowCount === 2) return [firstRow, secondRow];
 
     if (rowCount === 3)
         return [firstRow, secondRow, [1, secondRow[0] + secondRow[1], 1]];
-
-    if (rowCount === 1) return [firstRow];
 
     return triangle;
 }
