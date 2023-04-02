fb46243f3bade7f8dbc2c92a217fa29adf1bc504
Adam Whaley <adam@industriallogic.com>
2023-04-02 10:25:44 -0500

    add test and functionality for row of 1

--- typescript/PascalsTriangle/src/index.ts
+++ typescript/PascalsTriangle/src/index.ts
@@ -1,3 +1,4 @@
 export function generateTriangle(number: number) {
+    if (number === 1) return [[1]];
     return [];
 }
--- typescript/PascalsTriangle/tests/index.spec.ts
+++ typescript/PascalsTriangle/tests/index.spec.ts
@@ -7,4 +7,7 @@ describe("Pascal's Triangle", () => {
     it("Produces an empty list for 0", () => {
         expect(generateTriangle(0)).to.deep.eq([]);
     });
+    it("Produces first row for number of rows 1", () => {
+        expect(generateTriangle(1)).to.deep.eq([[1]]);
+    });
 });
