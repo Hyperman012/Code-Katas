c03c850020ecbbd3f0c091d16b49db979c2fdb8a
Adam Whaley <adam@industriallogic.com>
2023-04-16 10:28:30 -0500

    start parallel change for Shape enum

--- typescript/RockPaperScissors/src/index.ts
+++ typescript/RockPaperScissors/src/index.ts
@@ -1,8 +1,13 @@
 export class Round {
-    constructor(private ourPlay: string) {}
+    constructor(private ourPlay: string, private ourShape: Shape) {}
 
     score(): number {
         if (this.ourPlay === "Y") return 2;
         return 1;
     }
+}
+
+export enum Shape {
+    Rock,
+    Paper,
 }
--- typescript/RockPaperScissors/tests/index.spec.ts
+++ typescript/RockPaperScissors/tests/index.spec.ts
@@ -1,12 +1,12 @@
 import { expect } from "chai";
 import "mocha";
-import { Round } from "../src";
+import { Round, Shape } from "../src";
 
 describe("Rock Paper Scissors", () => {
     it("returns 1 for choosing rock", () => {
-        expect(new Round("X").score()).to.eq(1);
+        expect(new Round("X", Shape.Rock).score()).to.eq(1);
     });
     it("returns 2 for choosing paper", () => {
-        expect(new Round("Y").score()).to.eq(2);
+        expect(new Round("Y", Shape.Paper).score()).to.eq(2);
     });
 });
