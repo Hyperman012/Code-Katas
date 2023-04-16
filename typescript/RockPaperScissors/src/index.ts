02103ed1fa2279ebd6ec0181feff77f48e7a03e3
Adam Whaley <adam@industriallogic.com>
2023-04-16 10:23:13 -0500

    adding case to score paper

--- typescript/RockPaperScissors/src/index.ts
+++ typescript/RockPaperScissors/src/index.ts
@@ -1,5 +1,8 @@
 export class Round {
+    constructor(private ourPlay: string) {}
+
     score(): number {
+        if (this.ourPlay === "Y") return 2;
         return 1;
     }
 }
--- typescript/RockPaperScissors/tests/index.spec.ts
+++ typescript/RockPaperScissors/tests/index.spec.ts
@@ -2,8 +2,11 @@ import { expect } from "chai";
 import "mocha";
 import { Round } from "../src";
 
-describe("default", () => {
+describe("Rock Paper Scissors", () => {
     it("returns 1 for choosing rock", () => {
-        expect(new Round().score()).to.eq(1);
+        expect(new Round("X").score()).to.eq(1);
+    });
+    it("returns 2 for choosing paper", () => {
+        expect(new Round("Y").score()).to.eq(2);
     });
 });
