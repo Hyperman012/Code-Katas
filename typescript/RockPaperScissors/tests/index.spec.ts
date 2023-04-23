2f9ca9a0def3c1ddf38b8ad9e504fa687fcf7d13
Adam Whaley <adam@industriallogic.com>
2023-04-23 10:38:54 -0500

    wrap shape tests in describe

--- typescript/RockPaperScissors/tests/index.spec.ts
+++ typescript/RockPaperScissors/tests/index.spec.ts
@@ -3,38 +3,40 @@ import "mocha";
 import { Round, HandShape, Shape } from "../src";
 
 describe("Rock Paper Scissors", () => {
-    [
-        { score: 3, shape: HandShape.Scissors },
-        { score: 2, shape: HandShape.Paper },
-        { score: 1, shape: HandShape.Rock },
-    ].forEach(({ score, shape }) => {
-        it(`returns ${score} for choosing ${shape}`, () => {
-            expect(new Shape(shape).score()).to.eq(score);
+    describe("Shape", () => {
+        [
+            { score: 3, shape: HandShape.Scissors },
+            { score: 2, shape: HandShape.Paper },
+            { score: 1, shape: HandShape.Rock },
+        ].forEach(({ score, shape }) => {
+            it(`returns ${score} for choosing ${shape}`, () => {
+                expect(new Shape(shape).score()).to.eq(score);
+            });
         });
-    });
 
-    it("is paper shape", () => {
-        const paper = Shape.Paper;
-        expect(paper.isPaper()).to.be.true;
-        expect(paper.isRock()).to.be.false;
-        expect(paper.isScissors()).to.be.false;
-    });
-    it("is rock shape", () => {
-        const rock = Shape.Rock;
-        expect(rock.isPaper()).to.be.false;
-        expect(rock.isRock()).to.be.true;
-        expect(rock.isScissors()).to.be.false;
-    });
-    it("is scissors shape", () => {
-        const scissors = Shape.Scissors;
-        expect(scissors.isPaper()).to.be.false;
-        expect(scissors.isRock()).to.be.false;
-        expect(scissors.isScissors()).to.be.true;
-    });
-    it("determines equality", () => {
-        expect(Shape.Rock.equals(Shape.Rock)).to.be.true;
-        expect(Shape.Rock.equals(Shape.Paper)).to.be.false;
-        expect(Shape.Rock.equals(Shape.Scissors)).to.be.false;
+        it("is paper shape", () => {
+            const paper = Shape.Paper;
+            expect(paper.isPaper()).to.be.true;
+            expect(paper.isRock()).to.be.false;
+            expect(paper.isScissors()).to.be.false;
+        });
+        it("is rock shape", () => {
+            const rock = Shape.Rock;
+            expect(rock.isPaper()).to.be.false;
+            expect(rock.isRock()).to.be.true;
+            expect(rock.isScissors()).to.be.false;
+        });
+        it("is scissors shape", () => {
+            const scissors = Shape.Scissors;
+            expect(scissors.isPaper()).to.be.false;
+            expect(scissors.isRock()).to.be.false;
+            expect(scissors.isScissors()).to.be.true;
+        });
+        it("determines equality", () => {
+            expect(Shape.Rock.equals(Shape.Rock)).to.be.true;
+            expect(Shape.Rock.equals(Shape.Paper)).to.be.false;
+            expect(Shape.Rock.equals(Shape.Scissors)).to.be.false;
+        });
     });
 
     [
