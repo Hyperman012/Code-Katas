f9bcdcc0d1c3ca25dff56210d1f7799dbdd1ae0e
Adam Whaley <adam@industriallogic.com>
2023-05-21 10:15:16 -0500

    reorganized describe blocks

--- typescript/RockPaperScissors/tests/index.spec.ts
+++ typescript/RockPaperScissors/tests/index.spec.ts
@@ -3,44 +3,42 @@ import "mocha";
 import { Round } from "../src";
 import { HandShape, Shape } from "../src/Shape";
 import { RoundResult } from "../src/RoundResult";
-
-describe("Rock Paper Scissors", () => {
-    describe("Shape", () => {
-        [
-            { score: 3, shape: HandShape.Scissors },
-            { score: 2, shape: HandShape.Paper },
-            { score: 1, shape: HandShape.Rock },
-        ].forEach(({ score, shape }) => {
-            it(`returns ${score} for choosing ${shape}`, () => {
-                expect(new Shape(shape).score()).to.eq(score);
-            });
-        });
-
-        it("is paper shape", () => {
-            const paper = Shape.Paper;
-            expect(paper.isPaper()).to.be.true;
-            expect(paper.isRock()).to.be.false;
-            expect(paper.isScissors()).to.be.false;
-        });
-        it("is rock shape", () => {
-            const rock = Shape.Rock;
-            expect(rock.isPaper()).to.be.false;
-            expect(rock.isRock()).to.be.true;
-            expect(rock.isScissors()).to.be.false;
-        });
-        it("is scissors shape", () => {
-            const scissors = Shape.Scissors;
-            expect(scissors.isPaper()).to.be.false;
-            expect(scissors.isRock()).to.be.false;
-            expect(scissors.isScissors()).to.be.true;
-        });
-        it("determines equality", () => {
-            expect(Shape.Rock.equals(Shape.Rock)).to.be.true;
-            expect(Shape.Rock.equals(Shape.Paper)).to.be.false;
-            expect(Shape.Rock.equals(Shape.Scissors)).to.be.false;
+describe("Shape", () => {
+    [
+        { score: 3, shape: HandShape.Scissors },
+        { score: 2, shape: HandShape.Paper },
+        { score: 1, shape: HandShape.Rock },
+    ].forEach(({ score, shape }) => {
+        it(`returns ${score} for choosing ${shape}`, () => {
+            expect(new Shape(shape).score()).to.eq(score);
         });
     });
 
+    it("is paper shape", () => {
+        const paper = Shape.Paper;
+        expect(paper.isPaper()).to.be.true;
+        expect(paper.isRock()).to.be.false;
+        expect(paper.isScissors()).to.be.false;
+    });
+    it("is rock shape", () => {
+        const rock = Shape.Rock;
+        expect(rock.isPaper()).to.be.false;
+        expect(rock.isRock()).to.be.true;
+        expect(rock.isScissors()).to.be.false;
+    });
+    it("is scissors shape", () => {
+        const scissors = Shape.Scissors;
+        expect(scissors.isPaper()).to.be.false;
+        expect(scissors.isRock()).to.be.false;
+        expect(scissors.isScissors()).to.be.true;
+    });
+    it("determines equality", () => {
+        expect(Shape.Rock.equals(Shape.Rock)).to.be.true;
+        expect(Shape.Rock.equals(Shape.Paper)).to.be.false;
+        expect(Shape.Rock.equals(Shape.Scissors)).to.be.false;
+    });
+});
+describe("Rock Paper Scissors", () => {
     [
         {
             score: 0 + 3,
@@ -69,22 +67,21 @@ describe("Rock Paper Scissors", () => {
             );
         });
     });
-
-    describe("Round Result", () => {
-        it("determines tie", () => {
-            expect(new RoundResult(Shape.Scissors, Shape.Scissors).isTie()).to
-                .be.true;
-        });
-        it("determines winner for Paper vs Rock", () => {
-            expect(new RoundResult(Shape.Paper, Shape.Rock).weWin()).to.be.true;
-        });
-        it("determines winner for Scissors vs Paper", () => {
-            const weWin = new RoundResult(Shape.Scissors, Shape.Paper).weWin();
-            expect(weWin).to.be.true;
-        });
-        it("determines loser for Scissors vs Rock", () => {
-            const weWin = new RoundResult(Shape.Scissors, Shape.Rock).weWin();
-            expect(weWin).to.be.false;
-        });
+});
+describe("Round Result", () => {
+    it("determines tie", () => {
+        expect(new RoundResult(Shape.Scissors, Shape.Scissors).isTie()).to.be
+            .true;
+    });
+    it("determines winner for Paper vs Rock", () => {
+        expect(new RoundResult(Shape.Paper, Shape.Rock).weWin()).to.be.true;
+    });
+    it("determines winner for Scissors vs Paper", () => {
+        const weWin = new RoundResult(Shape.Scissors, Shape.Paper).weWin();
+        expect(weWin).to.be.true;
+    });
+    it("determines loser for Scissors vs Rock", () => {
+        const weWin = new RoundResult(Shape.Scissors, Shape.Rock).weWin();
+        expect(weWin).to.be.false;
     });
 });
