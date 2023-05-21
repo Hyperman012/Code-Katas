825084f9fdc896215952d3943182345f79c3240b
Adam Whaley <adam@industriallogic.com>
2023-05-21 10:12:12 -0500

    extract roundResult as field of Round

--- typescript/RockPaperScissors/src/index.ts
+++ typescript/RockPaperScissors/src/index.ts
@@ -1,25 +1,22 @@
 import { Shape } from "./Shape";
 
 export class Round {
+    private readonly roundResult = new RoundResult(
+        this.ourShape,
+        this.opponentShape
+    );
+
     constructor(private ourShape: Shape, private opponentShape: Shape) {}
 
     totalScore() {
-        if (this.weWin()) {
+        if (this.roundResult.weWin()) {
             return this.ourShape.score() + 6;
         }
 
-        if (this.isTie()) {
+        if (this.roundResult.isTie()) {
             return this.ourShape.score() + 3;
         }
         return this.ourShape.score();
-    }
-
-    private weWin() {
-        return new RoundResult(this.ourShape, this.opponentShape).weWin();
-    }
-
-    private isTie() {
-        return new RoundResult(this.ourShape, this.opponentShape).isTie();
     }
 }
 
