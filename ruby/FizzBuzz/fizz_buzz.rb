656abac333c7221a1d66a01ece1307ac4a25b694
Adam Whaley <adam@industriallogic.com>
2023-07-21 18:29:15 -0500

    basic ruby fizzBuzz project

---
+++ ruby/FizzBuzz/fizz_buzz.rb
@@ -0,0 +1,8 @@
+class FizzBuzz
+  def process(input)
+    return "fizz" if input.equal? 3
+
+    input.to_s
+  end
+
+end
---
+++ ruby/FizzBuzz/spec/fizz_buzz_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../fizz_buzz'
+require 'rspec'
+
+describe FizzBuzz do
+  it 'returns string 1 for 1' do
+    fb= FizzBuzz.new
+    expect(fb.process(1)).to eq '1'
+  end
+
+  it 'returns string 2 for 2' do
+    fb= FizzBuzz.new
+    expect(fb.process(2)).to eq '2'
+  end
+
+  it 'returns fizz for 3' do
+    fb= FizzBuzz.new
+    expect(fb.process(3)).to eq 'fizz'
+
+  end
+end
