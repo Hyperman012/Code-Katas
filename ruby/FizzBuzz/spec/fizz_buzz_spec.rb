6ab92771d8846e5d7ed499402886eacd9fadd935
Adam Whaley <adam@industriallogic.com>
2023-07-23 10:33:16 -0500

    paramarize tests

--- ruby/FizzBuzz/spec/fizz_buzz_spec.rb
+++ ruby/FizzBuzz/spec/fizz_buzz_spec.rb
@@ -3,37 +3,26 @@ require 'rspec'
 
 describe FizzBuzz do
   fb = FizzBuzz.new
-  
-  it 'return "1" for 1'  do
-    expect(fb.process(1)).to eq '1'
-  end
-
-  it 'return "2" for 2'  do
-    expect(fb.process(2)).to eq '2'
-  end
-
-  it 'return "fizz" for 3' do
-    expect(fb.process(3)).to eq 'fizz'
-  end
-
-  it 'return "fizz" for 6' do
-    expect(fb.process(6)).to eq 'fizz'
-  end
-
-  it 'return "buzz" for 5' do
-    expect(fb.process(5)).to eq 'buzz'
-  end
-
-  it 'return "buzz" for 10' do
-    expect(fb.process(10)).to eq 'buzz'
+  [1, 2].each { |x|
+    it "return '#{x}' for #{x}" do
+      expect(fb.process(x)).to eq x.to_s
     end
-
-  it 'return "fizzbuzz" for 15' do
-    expect(fb.process(15)).to eq 'fizzbuzz'
+  }
+  [3, 6].each { |x|
+    it "return 'fizz' for #{x}" do
+      expect(fb.process(x)).to eq 'fizz'
     end
+  }
 
-  it 'return "fizzbuzz" for 30' do
-    expect(fb.process(30)).to eq 'fizzbuzz'
-  end
+  [5, 10].each { |number|
+    it "return 'buzz' for #{number}" do
+      expect(fb.process(number)).to eq 'buzz'
+    end
+  }
 
+  [15, 30].each { |number|
+    it "return \"fizzbuzz\" for #{number}" do
+      expect(fb.process(number)).to eq 'fizzbuzz'
+    end
+  }
 end
