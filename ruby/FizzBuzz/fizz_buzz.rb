da59a6fc992189520bba636bf7e70421ae2ce1d4
Adam Whaley <adam@industriallogic.com>
2023-07-23 10:27:00 -0500

    refactor;

--- ruby/FizzBuzz/fizz_buzz.rb
+++ ruby/FizzBuzz/fizz_buzz.rb
@@ -1,10 +1,15 @@
-class FizzBuzz
-  def process(input)
-    return "fizzbuzz" if input % 15 == 0
-    return "buzz" if input % 5 == 0
-    return "fizz" if input % 3 == 0
-
-    input.to_s
+class Integer
+  def isDivisibleBy(integer)
+    self % integer == 0
   end
 
+end
+
+class FizzBuzz
+  def process(input)
+    return "fizzbuzz" if input.isDivisibleBy(15)
+    return "buzz" if input.isDivisibleBy(5)
+    return "fizz" if input.isDivisibleBy(3)
+    input.to_s
+  end
 end
