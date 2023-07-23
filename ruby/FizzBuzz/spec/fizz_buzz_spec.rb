a1252811c5a4f7b8519e9c09bea6978b16834051
Adam Whaley <adam@industriallogic.com>
2023-07-23 10:28:03 -0500

    move fizzbuzz new up

--- ruby/FizzBuzz/spec/fizz_buzz_spec.rb
+++ ruby/FizzBuzz/spec/fizz_buzz_spec.rb
@@ -2,43 +2,37 @@ require_relative '../fizz_buzz'
 require 'rspec'
 
 describe FizzBuzz do
+  fb = FizzBuzz.new
+  
   it 'return "1" for 1'  do
-    fb = FizzBuzz.new
     expect(fb.process(1)).to eq '1'
   end
 
   it 'return "2" for 2'  do
-    fb = FizzBuzz.new
     expect(fb.process(2)).to eq '2'
   end
 
   it 'return "fizz" for 3' do
-    fb = FizzBuzz.new
     expect(fb.process(3)).to eq 'fizz'
   end
 
   it 'return "fizz" for 6' do
-    fb = FizzBuzz.new
     expect(fb.process(6)).to eq 'fizz'
   end
 
   it 'return "buzz" for 5' do
-    fb = FizzBuzz.new
     expect(fb.process(5)).to eq 'buzz'
   end
 
   it 'return "buzz" for 10' do
-    fb = FizzBuzz.new
     expect(fb.process(10)).to eq 'buzz'
     end
 
   it 'return "fizzbuzz" for 15' do
-    fb = FizzBuzz.new
     expect(fb.process(15)).to eq 'fizzbuzz'
     end
 
   it 'return "fizzbuzz" for 30' do
-    fb = FizzBuzz.new
     expect(fb.process(30)).to eq 'fizzbuzz'
   end
 
