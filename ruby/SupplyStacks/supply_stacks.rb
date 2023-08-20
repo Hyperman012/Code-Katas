8b40f38944a8cddb543225f45fbee048fa425119
Adam Whaley <adam@industriallogic.com>
2023-08-20 15:07:15 -0500

    extract strip crate line

--- ruby/SupplyStacks/supply_stacks.rb
+++ ruby/SupplyStacks/supply_stacks.rb
@@ -7,10 +7,16 @@ class SupplyStacks
   def stacks
     lines = @input.split("\n")
     first_row = lines[0]
-    foo = first_row.delete("[").delete("]")
 
-    pants = foo.split(" ")
+    pants = strip_crate_line(first_row)
+
     pants.map { |crate| [crate]  }
+  end
+
+  private
+
+  def strip_crate_line(row)
+    row.delete("[").delete("]").split(" ")
   end
 
 end
