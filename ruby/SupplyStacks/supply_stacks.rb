47710e73acce37c0b2777ffbedbd196d89f25b25
Adam Whaley <adam@industriallogic.com>
2023-08-20 15:07:51 -0500

    rename to first_crate_line

--- ruby/SupplyStacks/supply_stacks.rb
+++ ruby/SupplyStacks/supply_stacks.rb
@@ -8,9 +8,9 @@ class SupplyStacks
     lines = @input.split("\n")
     first_row = lines[0]
 
-    pants = strip_crate_line(first_row)
+    first_crate_line = strip_crate_line(first_row)
 
-    pants.map { |crate| [crate]  }
+    first_crate_line.map { |crate| [crate]  }
   end
 
   private
