7e063b2c38f494e500679f6086ce45c34517183f
Adam Whaley <adam@industriallogic.com>
2024-02-12 13:06:05 -0600

    solution

--- typescript/FizzBuzz/src/index.ts
+++ typescript/FizzBuzz/src/index.ts
@@ -1,3 +1,19 @@
-export function calculate(a : number) : string {
-    return ''
+function isDivisibleBy5(input: number):boolean {
+    return isDivisibleBy(5, input)
+}
+
+function isDivisibleBy3(input: number):boolean {
+    return isDivisibleBy(3, input)
+}
+
+function isDivisibleBy(divisor:number, input:number):boolean   {
+    return input % divisor ===0;
+}
+
+export function calculate(input : number) : string {
+    if( isDivisibleBy5(input) && isDivisibleBy3(input) ) return 'fizzbuzz'
+    if (isDivisibleBy5(input) ) return 'buzz';
+    if (isDivisibleBy3(input) ) return 'fizz';
+
+    return String(input)
 }
--- typescript/FizzBuzz/tests/index.spec.ts
+++ typescript/FizzBuzz/tests/index.spec.ts
@@ -1,7 +1,28 @@
 import {  calculate } from '../src';
 
-describe('test', () => {
-    it('adds 1 + 2', () => {
-        expect(calculate(1)).toEqual('')
+describe('calculate', () => {
+
+    [1,2,4].forEach(input => {
+        it(`returns "${input}" for ${input}`, () => {
+            expect(calculate(input)).toEqual(String(input))
+        });
     });
+
+    [3,6,9].forEach(input => {
+        it(`returns "fizz" for ${input}`, () => {
+            expect(calculate(input)).toEqual('fizz')
+        });
+    });
+
+    [5,10,20].forEach(input => {
+        it(`returns "buzz" for ${input}`, () => {
+            expect(calculate(input)).toEqual('buzz')
+        });
+    });
+    [15,30,45].forEach(input => {
+        it(`returns "fizzbuzz" for ${input}`, () => {
+            expect(calculate(input)).toEqual('fizzbuzz')
+        });
+    });
+
 });
