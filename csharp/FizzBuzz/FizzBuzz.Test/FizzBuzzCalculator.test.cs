5fbba18a2de337ec3560b8653ce29fb5740621af
Adam Whaley <whaley@Mac.localdomain>
2025-03-03 09:37:35 -0600

    .gitignore added

---
+++ csharp/FizzBuzz/FizzBuzz.Test/FizzBuzzCalculator.test.cs
@@ -0,0 +1,20 @@
+namespace FizzBuzz.Test;
+/*
+* write some code when given a number,
+* returns a string of that number unless:
+* the input is a multiple of 3, in which case return Fizz
+* or
+* the input is a multiple of 5, in which case return Buzz
+* or
+* the input is a multiple of 3 and 5, in which case return FizzBuzz
+*/
+public class FizzBuzzCalculatorTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var sut = new FizzBuzzCalculator();
+        int result = sut.Add(1, 2);
+        Assert.Equal(3, result);
+    }
+}
