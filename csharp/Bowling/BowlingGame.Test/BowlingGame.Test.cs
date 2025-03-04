18956f59e4c52d2497e06093d7cba493ec13bb57
Adam Whaley <abwn83@gmail.com>
2025-03-04 11:32:02 -0500

    get bowling scaffold up and running!

---
+++ csharp/Bowling/BowlingGame.Test/BowlingGame.Test.cs
@@ -0,0 +1,20 @@
+namespace Default.Test;
+/*
+* write something when given a number,
+* returns a string of that number unless:
+* the input is a multiple of 3, in which case return Fizz
+* or
+* the input is a multiple of 5, in which case return Buzz
+* or
+* the input is a multiple of 3 and 5, in which case return BowlingGame
+*/
+public class BowlingGameTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var sut = new BowlingGame();
+        int result = sut.Add(1, 2);
+        Assert.Equal(3, result);
+    }
+}
