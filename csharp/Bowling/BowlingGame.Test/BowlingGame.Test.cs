b94cb91b163e3cb12638703d331212ab0664f1fa
Adam Whaley <abwn83@gmail.com>
2025-03-26 12:36:41 -0500

    solution

--- csharp/Bowling/BowlingGame.Test/BowlingGame.Test.cs
+++ csharp/Bowling/BowlingGame.Test/BowlingGame.Test.cs
@@ -1,11 +1,81 @@
 namespace Default.Test;
 public class BowlingGameTest
 {
+    private Game _game = new();
+
     [Fact]
-    public void Test1()
+    public void Score_GameStart_ReturnsZero()
     {
-        var sut = new Game();
-        int result = sut.score();
+        int result = _game.Score();
         Assert.Equal(0, result);
+    }
+    
+    [Fact]
+    public void Score_FirstRollOfOne_ReturnsOne()
+    {
+        _game.Roll(1);
+        int result = _game.Score();
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Score_FirstRollOfTwo_ReturnsTwo()
+    {
+        _game.Roll(2);
+        int result = _game.Score();
+        Assert.Equal(2, result);
+    }
+    
+    [Fact]
+    public void Score_FirstRollOfThree_ReturnsThree()
+    {
+        _game.Roll(3);
+        int result = _game.Score();
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void Score_RollsOfOneAndTwo_ReturnsThree()
+    {
+        _game.Roll(1);
+        _game.Roll(2);
+        int result = _game.Score();
+        Assert.Equal(3, result);
+    }
+ 
+    [Fact]
+    public void Score_WithSpare_Returns16()
+    {
+        _game.Roll(5);
+        _game.Roll(5); // spare
+        _game.Roll(3);
+        int result = _game.Score();
+        Assert.Equal(16, result);
+    }
+
+    [Fact]
+    public void Score_WithStrike_returns17()
+    {
+        _game.Roll(10);
+        _game.Roll(4); // spare
+        _game.Roll(3);
+        int result = _game.Score();
+        Assert.Equal(24, result);
+    } 
+    
+    [Fact]
+    public void Score_PerfectGame_Returns300()
+    {
+        RollMany(10, 20);
+        int result = _game.Score();
+        Assert.Equal(300, result);
+    }
+
+    private void RollMany(int pinCount, int howMany)
+    {
+        for (int i = 0; i < howMany; i++)
+        {
+            _game.Roll(pinCount);
+        }
     }
 }
--- csharp/Bowling/BowlingGame/BowlingGame.cs
+++ csharp/Bowling/BowlingGame/BowlingGame.cs
@@ -2,13 +2,45 @@
 
 public class Game
 {
-    public void roll(int pins)
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void Roll(int pins)
     {
-    
+        rolls[currentRoll++] = pins;
     }
 
-    public int score()
+    public int Score()
     {
-        return 0;
+        var score = 0;
+        int rollIndex = 0;
+        for (var frame = 0; frame < 10 ; frame++)
+        {
+            if (rolls[rollIndex] == 10) //strike
+            {
+                score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                rollIndex++;
+            }
+            else if (IsSpare(rollIndex)) // spare
+            {
+                score += 10 + rolls[rollIndex + 2];
+                rollIndex += 2;
+            }
+            else
+            {
+                score += rolls[rollIndex] + rolls[rollIndex + 1];
+                rollIndex += 2;
+            }
+
+        }
+
+        return score;
+    }
+
+    private bool IsSpare(int rollIndex)
+    {
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
     }
 }
+
+
