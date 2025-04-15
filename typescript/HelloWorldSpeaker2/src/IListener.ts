c4b25c4bb30b9228ad9a5ac32357e80f3e351534
Adam Whaley <abwn83@gmail.com>
2025-04-15 14:02:11 -0400

    add hello world speaker2 to TS

---
+++ typescript/HelloWorldSpeaker2/src/IListener.ts
@@ -0,0 +1,6 @@
+export interface IListener
+{
+    Listen(whatISay: string): void;
+}
+
+
---
+++ typescript/HelloWorldSpeaker2/src/RealListener.ts
@@ -0,0 +1,12 @@
+import { IListener } from "./IListener";
+
+export class RealListener implements IListener {
+
+    Listen(whatISay: string): void {
+        /*
+        * Enormously complex code that does strange
+        * and wonderous things goes here!
+        */
+    }
+
+}
